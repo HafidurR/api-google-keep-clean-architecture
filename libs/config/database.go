9182b1ecee4f95dd3c4b5fcc92eea7b1ba516013
HafidurR <deifah126@gmail.com>
2023-03-18 10:34:10 +0700

    edit env config

--- libs/config/database.go
+++ libs/config/database.go
@@ -3,17 +3,20 @@ package config
 import (
 	"fmt"
 
+	"api-google-keep/core/entity"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func Init(cfg Config) *gorm.DB {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%v)/%s?charset=utf8mb4&parseTime=True&loc=Local", cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.DatabaseName)
- 
+	
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic(err)
+		fmt.Println(err.Error())
 	}
 
+	db.AutoMigrate(&entity.User{}, &entity.Note{})
+ 
 	return db
 }
